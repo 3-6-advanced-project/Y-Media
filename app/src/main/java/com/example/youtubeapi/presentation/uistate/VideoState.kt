f37afe655bfe37ef7e21192af19db88ef75808a7
rlaxodud214 <rlaxodud214@naver.com>
2024-05-16 18:37:49 +0900

    refactor: 인기 동영상 DTO와 동영상 검색 DTO 분리 및 기존 코드와 연동

--- app/src/main/java/com/example/youtubeapi/presentation/uistate/VideoState.kt
+++ app/src/main/java/com/example/youtubeapi/presentation/uistate/VideoState.kt
@@ -1,7 +1,8 @@
 package com.example.youtubeapi.presentation.uistate
 
+import com.example.youtubeapi.data.model.dto.HomeVideo
+import com.example.youtubeapi.data.model.dto.SearchVideo
 import com.example.youtubeapi.data.model.dto.Thumbnail
-import com.example.youtubeapi.data.model.dto.Video
 
 data class VideoState(
     val id: String,
@@ -9,21 +10,30 @@ data class VideoState(
     val description: String,
     val channelTitle: String,
     val thumbnail: Thumbnail,
-    val duration: String,
-    val categoryId: String,
+    val duration: String = "",
+    val categoryId: String = "",
     val publishedAt: String,
 )
 
 /**
  * thumbnail resolution default
  * */
-fun Video.asVideoState() = VideoState(
-    id = id.videoId,
+fun HomeVideo.asVideoState() = VideoState(
+    id = id,
     title = snippet.title,
     description = snippet.description,
     channelTitle = snippet.channelTitle,
     thumbnail = snippet.thumbnails.default,
     duration = contentDetails.duration,
     categoryId = snippet.categoryId,
+    publishedAt = snippet.publishedAt
+)
+
+fun SearchVideo.asVideoState() = VideoState(
+    id = id.videoId,
+    title = snippet.title,
+    description = snippet.description,
+    channelTitle = snippet.channelTitle,
+    thumbnail = snippet.thumbnails.default,
     publishedAt = snippet.publishedAt
 )
