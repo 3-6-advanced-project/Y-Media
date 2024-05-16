b3babd80f2bd6b63424f67bcfb12560a66eff392
SooyoungHwang <75528131+agriades@users.noreply.github.com>
2024-05-16 09:52:06 +0900

    feat: 각 tab 선택 시 아이콘 파일 변경 (#3)
    * add: 'home', 'search', 'likes' icons resource
    
    * fix: 개인 브랜치 local.properties, deploymentTargetDropDown.xml 수정
    
    * style : tabLayout 색깔 지정
    
    * feat : tab 선택 시 아이콘 파일 변경
    
    * feat : tab 선택 시 아이콘 파일 변경
    
    * style: 선택된 tab 아이콘 색상 변경
    
    * style: 선택된 tab 아이콘 색상 변경
    
    * refactor: 아이콘도 selector 사용

--- app/src/main/java/com/example/youtubeapi/presentation/MainActivity.kt
+++ app/src/main/java/com/example/youtubeapi/presentation/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.youtubeapi.presentation
 
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.youtubeapi.HomeFragment_TAG
 import com.example.youtubeapi.MyVideoFragment_TAG
 import com.example.youtubeapi.R
 import com.example.youtubeapi.SearchFragment_TAG
 import com.example.youtubeapi.adapter.ViewPagerAdapter
 import com.example.youtubeapi.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,17 +27,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initView() {
-
         with(binding) {
             vp.adapter = ViewPagerAdapter(this@MainActivity)
             TabLayoutMediator(tl, vp) { tab, position ->
                 when(position) {
                     0 -> {
-                        tab.setIcon(R.drawable.baseline_home_black_24dp)
+                        tab.setIcon(R.drawable.selector_home) //초기 화면. 그런데 이렇게 코드를 짰을 때 문제는 다른 탭/뷰페이저가 지정된 상태에서 앱이 시작했을 때다.
                         tab.text = HomeFragment_TAG
                     }
-                    1 -> tab.text = SearchFragment_TAG
-                    2 -> tab.text = MyVideoFragment_TAG
+                    1 -> {
+                        tab.setIcon(R.drawable.selector_likes)
+                        tab.text = SearchFragment_TAG
+                    }
+                    2 -> {
+                        tab.setIcon(R.drawable.selector_search)
+                        tab.text = MyVideoFragment_TAG
+                    }
                 }
             }.attach()
         }
