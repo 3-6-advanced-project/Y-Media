3a9d17e7a1362f924b192bb795900e8b45f4690c
treeralph <treeralph@gmail.com>
2024-05-16 16:12:59 +0900

    feat: VideoCategory List API를 위한 GoogleApiDataSource에 getVideoCategories 함수, VideoRepository  getVideoCategories함수 작성 및 VideoCategoryResponse, VideoCategory DTO

--- app/src/main/java/com/example/youtubeapi/data/remote/GoogleApiDataSource.kt
+++ app/src/main/java/com/example/youtubeapi/data/remote/GoogleApiDataSource.kt
@@ -1,6 +1,7 @@
 package com.example.youtubeapi.data.remote
 
 import com.example.youtubeapi.BuildConfig
+import com.example.youtubeapi.data.model.dto.VideoCategoryResponse
 import com.example.youtubeapi.data.model.dto.VideoResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -18,4 +19,11 @@ interface GoogleApiDataSource {
         @Query("regionCode") regionCode: String = "KR",
         @Query("videoCategoryId") videoCategoryId: String,
     ): VideoResponse
+
+    @GET("youtube/v3/videoCategories")
+    suspend fun getVideoCategories(
+        @Query("key") key: String = BuildConfig.GOOGLE_API_KEY,
+        @Query("part") part: String = "snippet",
+        @Query("regionCode") regionCode: String = "KR"
+    ): VideoCategoryResponse
 }
