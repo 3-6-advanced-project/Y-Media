8447c12d9b53d40200d4f78c816af4557a0a9e9d
rlaxodud214 <rlaxodud214@naver.com>
2024-05-16 18:37:49 +0900

    refactor: 인기 동영상 DTO와 동영상 검색 DTO 분리 및 기존 코드와 연동

--- app/src/main/java/com/example/youtubeapi/data/remote/GoogleApiDataSource.kt
+++ app/src/main/java/com/example/youtubeapi/data/remote/GoogleApiDataSource.kt
@@ -1,6 +1,8 @@
 package com.example.youtubeapi.data.remote
 
 import com.example.youtubeapi.BuildConfig
+import com.example.youtubeapi.data.model.dto.HomeVideo
+import com.example.youtubeapi.data.model.dto.SearchVideo
 import com.example.youtubeapi.data.model.dto.VideoResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -17,5 +19,18 @@ interface GoogleApiDataSource {
         @Query("maxWidth") maxWidth: Int,
         @Query("regionCode") regionCode: String = "KR",
         @Query("videoCategoryId") videoCategoryId: String,
-    ): VideoResponse
+    ): VideoResponse<HomeVideo>
+
+    @GET("youtube/v3/search")
+    suspend fun getSearchVideo(
+        @Query("key") key: String = BuildConfig.GOOGLE_API_KEY,
+        @Query("q") query: String, // 검색어
+        @Query("maxResults") maxResults: Int,
+        @Query("videoDefinition") videoDefinition: String,
+        @Query("type") type: String = "video", // video, channel, playlist
+        @Query("part") part: String = "snippet",
+        @Query("regionCode") regionCode: String = "KR",
+        // @Query("relevanceLanguage") relevanceLanguage: String = "KR", // 지정된 언어와 가장 관련성이 높은 검색결과를 반환
+
+    ): VideoResponse<SearchVideo>
 }
