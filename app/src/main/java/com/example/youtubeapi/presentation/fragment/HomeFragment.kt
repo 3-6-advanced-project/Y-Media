5efc3249ee8b2305ca5b41307da7a893eb1f72cb
treeralph <treeralph@gmail.com>
2024-05-17 11:25:29 +0900

    feat: Data - UIState 연결 작성

--- app/src/main/java/com/example/youtubeapi/presentation/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/youtubeapi/presentation/fragment/HomeFragment.kt
@@ -6,14 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import com.example.youtubeapi.R
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.youtubeapi.databinding.FragmentHomeBinding
+import com.example.youtubeapi.presentation.adapter.HomeRecyclerViewAdapter
+import com.example.youtubeapi.presentation.adapter.VideoClickListener
+import com.example.youtubeapi.presentation.adapter.decoration.ListItemDecoration
+import com.example.youtubeapi.presentation.uistate.VideoState
 import com.example.youtubeapi.viewmodel.MainViewModel
+import com.example.youtubeapi.viewmodel.MainViewModelFactory
+import kotlinx.coroutines.launch
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), VideoClickListener {
 
     private val binding by lazy { FragmentHomeBinding.inflate(layoutInflater) }
-    private val viewModel: MainViewModel by activityViewModels()
+    private val viewModel: MainViewModel by activityViewModels {
+        MainViewModelFactory()
+    }
+
+    private val mostPopularVideoAdapter by lazy { HomeRecyclerViewAdapter(this) }
+    private val categoryVideoAdapter by lazy { HomeRecyclerViewAdapter(this) }
+    private val categoryChannelAdapter by lazy { HomeRecyclerViewAdapter(this) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +37,42 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        with(binding) {
+            recyclerViewInit(rvMostPopularVideo)
+            recyclerViewInit(rvCategoryVideo)
+            recyclerViewInit(rvCategoryChannel)
+            rvMostPopularVideo.adapter = mostPopularVideoAdapter
+            rvCategoryVideo.adapter = categoryVideoAdapter
+            rvCategoryChannel.adapter = categoryChannelAdapter
+        }
+
+        lifecycleScope.launch {
+            viewModel.mostPopularVideos.collect {
+                mostPopularVideoAdapter.itemsUpdate(it)
+            }
+        }
+
+        lifecycleScope.launch {
+            viewModel.channels.collect {
+                /**
+                 * channel to video state
+                 * */
+
+            }
+        }
+    }
+
+    private fun recyclerViewInit(recyclerView: RecyclerView) {
+        recyclerView.layoutManager =
+            LinearLayoutManager(activity, RecyclerView.HORIZONTAL, false)
+        recyclerView.addItemDecoration(
+            ListItemDecoration(resources.displayMetrics.density).apply {
+                setPaddingValues(endDp = 8)
+            }
+        )
+    }
+
+    override fun onClick(videoState: VideoState) {
 
     }
 }
--- app/src/main/java/com/example/youtubeapi/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/youtubeapi/viewmodel/MainViewModel.kt
@@ -1,8 +1,70 @@
 package com.example.youtubeapi.viewmodel
 
 import androidx.lifecycle.ViewModel
-class MainViewModel: ViewModel() {
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.youtubeapi.data.model.dto.VideoCategory
+import com.example.youtubeapi.data.repository.VideoRepository
+import com.example.youtubeapi.network.RetrofitClient
+import com.example.youtubeapi.presentation.uistate.VideoState
+import com.example.youtubeapi.presentation.uistate.asVideoState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
+class MainViewModel(
+    private val videoRepository: VideoRepository
+): ViewModel() {
+
+    private val _mostPopularVideos = MutableStateFlow(listOf<VideoState>())
+    val mostPopularVideos = _mostPopularVideos.asStateFlow()
+
+    private val _mostPopularVideosWithCategory = MutableStateFlow(listOf<VideoState>())
+    val mostPopularVideoWithCategory = _mostPopularVideosWithCategory.asStateFlow()
+
+    private val _channels = MutableStateFlow(listOf<String>())
+    val channels = _channels.asStateFlow()
+
+    private val _categories = MutableStateFlow(listOf<VideoCategory>())
+    val categories = _categories.asStateFlow()
+
+    private val _currentCategory = MutableStateFlow<VideoCategory?>(null)
+    val currentCategory = _currentCategory.asStateFlow()
+
+    init {
+        initMostPopularVideos()
+        initVideoCategories()
+    }
+
+    private fun initMostPopularVideos() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _mostPopularVideos.value = videoRepository.getMostPopularVideo().items.map {
+                it.asVideoState()
+            }
+        }
+    }
+
+    private fun initVideoCategories() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _categories.value = videoRepository.getVideoCategories().items
+            initCurrentCategory(_categories.value[0])
+        }
+    }
+
+    private fun initCurrentCategory(videoCategory: VideoCategory) {
+        _currentCategory.value = videoCategory
+        viewModelScope.launch(Dispatchers.IO) {
+            videoRepository.getMostPopularVideo(videoCategoryId = videoCategory.id)
+        }
+    }
 }
 
+class MainViewModelFactory: ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return MainViewModel(
+            VideoRepository(RetrofitClient.googleApiSource)
+        ) as T
+    }
+}
 
