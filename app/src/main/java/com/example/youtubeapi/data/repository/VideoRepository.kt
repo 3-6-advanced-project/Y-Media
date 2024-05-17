d60373183cf3b5b2874f93c85e9916988568ed53
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-05-17 11:28:31 +0900

    feat: SearchListAdapter 및 SearchVideoAPI 연동 (#9)
    * feat: UI 구현
    
    * feat: search API 호출 코드 구현
    
    * feat: searchListAdapter 구현 및 연동
    
    * refactor: 프로젝트 패키지 구조 변경
    
    * refactor: rename VideoDao to VideoEntityDao
    
    * refactor(Video): Entity에서 DTO 분리
    
    * refactor: DataSource가 2개여서 이름 수정 RemoteDataSource to GoogleApiDataSource
    
    * add: Developer 경로의 api call을 위한 DataSource
    
    * feat(VideoState): UI 상태 관리를 위한 class 추가
    
    * refator: retrofit 객체 2개로 분리
    
    * fix: Api Response 중 Id Data Class 누락된 부분 추가
    
    * refactor: 인기 동영상과 동영상 검색 DTO 분리
    
    * refactor: 인기 동영상 DTO와 동영상 검색 DTO 분리 및 기존 코드와 연동
    
    * fix: binding 객체 초기화가 잘못되있던 오류 수정
    
    feat: coil 사용하여 이미지 url UI 연동
    
    * fix(xml): Search가 hint값이 아니라 text값으로 들어가고 있던 오류 수정
    
    * feat: 사용자 입력값을 기반으로 동영상을 검색하도록 구현
    
    feat: 키보드 상에서 Done 누르면 키보드 내려가고 SearchApi Run 되도록
    적용
    
    * refactor: 함수로 분리

--- app/src/main/java/com/example/youtubeapi/data/repository/VideoRepository.kt
+++ app/src/main/java/com/example/youtubeapi/data/repository/VideoRepository.kt
@@ -1,20 +1,32 @@
 package com.example.youtubeapi.data.repository
 
 import androidx.annotation.IntRange
+import com.example.youtubeapi.data.remote.DeveloperDataSource
 import com.example.youtubeapi.data.remote.GoogleApiDataSource
 
 class VideoRepository(
-    private val googleApiDataSource: GoogleApiDataSource
+    private val developerDataSource: DeveloperDataSource,
+    private val googleApiDataSource: GoogleApiDataSource,
 ) {
     suspend fun getMostPopularVideo(
         @IntRange(from = 72, to = 8192) maxHeight: Int = 512,
         @IntRange(from = 1, to = 50) maxResults: Int = 5,
         @IntRange(from = 72, to = 8192) maxWidth: Int = 512,
-        videoCategoryId: String = "0"
+        videoCategoryId: String = "0",
     ) = googleApiDataSource.getMostPopularVideo(
         maxHeight = maxHeight,
         maxResults = maxResults,
         maxWidth = maxWidth,
         videoCategoryId = videoCategoryId
+    )
+
+    suspend fun getSearchVideo(
+        query: String,
+        @IntRange(from = 1, to = 50) maxResults: Int = 50,
+        videoDefinition: String = "high",
+    ) = googleApiDataSource.getSearchVideo(
+        query = query,
+        maxResults = maxResults,
+        videoDefinition = videoDefinition,
     )
 }
--- app/src/main/java/com/example/youtubeapi/presentation/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/youtubeapi/presentation/fragment/SearchFragment.kt
@@ -1,19 +1,40 @@
 package com.example.youtubeapi.presentation.fragment
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.youtubeapi.R
+import com.example.youtubeapi.adapter.SearchListAdapter
 import com.example.youtubeapi.databinding.FragmentSearchBinding
+import com.example.youtubeapi.presentation.adapter.decoration.ListItemDecoration
+import com.example.youtubeapi.viewmodel.LatestNewsUiState
 import com.example.youtubeapi.viewmodel.MainViewModel
+import com.example.youtubeapi.viewmodel.MainViewModelFactory
+import kotlinx.coroutines.launch
 
 class SearchFragment : Fragment() {
-
     private val binding by lazy { FragmentSearchBinding.inflate(layoutInflater) }
-    private val viewModel: MainViewModel by activityViewModels()
+
+    private val viewModel: MainViewModel by activityViewModels() {
+        MainViewModelFactory()
+    }
+
+    private val searchListAdapter = SearchListAdapter() { video ->
+        // TODO : 이후 VideoDetailFragment()의 companion object{}에서 parameter를 받도록 수정되면 videoId값 넘겨줘야함
+        requireActivity().supportFragmentManager.beginTransaction()
+            // .replace(R.id.fl_item, VideoDetailFragment.newInstance(video.id))
+            .replace(R.id.ll_top, VideoDetailFragment())
+            .addToBackStack(null)
+            .commit()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +43,55 @@ class SearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initEditTextDoneActionListener()
+        initRecyclerView()
+        initViewModel()
+    }
+
+    private fun initEditTextDoneActionListener() = with(binding) {
+        etSearchKeyword.setOnEditorActionListener { v, actionId, event ->
+            var handled = false
+
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                // 키보드 내리기
+                val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(requireActivity().window.decorView.applicationWindowToken, 0)
+
+                viewModel.onSearch(etSearchKeyword.text.toString())
+                handled = true
+            }
+
+            handled
+        }
+    }
+
+
+    private fun initRecyclerView() = with(binding.rvSearch) {
+        adapter = searchListAdapter
+        layoutManager = LinearLayoutManager(requireContext())
+
+        addItemDecoration(
+            ListItemDecoration(resources.displayMetrics.density).apply {
+                setPaddingValues(bottomDp = 16)
+            }
+        )
+    }
+
+
+    private fun initViewModel() = lifecycleScope.launch {
+        viewModel.uiState.collect { uiState ->
+            when (uiState) {
+                is LatestNewsUiState.Success -> {
+                    val videoStates = uiState.videoStates
+                    searchListAdapter.submitList(videoStates.toMutableList())
+                }
+                is LatestNewsUiState.Error -> initRVItem()
+            }
+        }
+    }
+
+    private fun initRVItem() = with(binding) {
 
     }
 }
