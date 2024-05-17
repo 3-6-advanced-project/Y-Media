4fd3143e27077da7b80d2895bc423c7137db4ccb
treeralph <treeralph@gmail.com>
2024-05-17 21:29:27 +0900

    feat: VideoState와 VideoEntity 상호작용을 위한 insert & delete 수정

--- app/src/main/java/com/example/youtubeapi/data/local/dao/VideoEntityDao.kt
+++ app/src/main/java/com/example/youtubeapi/data/local/dao/VideoEntityDao.kt
@@ -9,8 +9,20 @@ import com.example.youtubeapi.data.model.entity.VideoEntity
 interface VideoEntityDao {
     @Query("SELECT * FROM VideoEntity")
     fun getAllVideoEntity(): List<VideoEntity>
-    @Delete
-    fun deleteVideoEntity(videoEntity: VideoEntity)
-    @Insert
-    fun insertVideoEntity(videoEntity: VideoEntity): Long
+    @Query("DELETE FROM VideoEntity WHERE videoId = :videoId")
+    fun deleteVideoEntityById(videoId: String)
+    @Query("INSERT INTO VideoEntity(videoId, title, description, channelTitle, " +
+            "channelId, publishedAt, duration, thumbnailUrl) " +
+            "VALUES(:videoId, :title, :description, :channelTitle, " +
+            ":channelId, :publishedAt, :duration, :thumbnailUrl)")
+    fun insertVideoEntityWithParameters(
+        videoId: String,
+        title: String,
+        description: String,
+        channelTitle: String,
+        channelId: String,
+        publishedAt: String,
+        duration: String,
+        thumbnailUrl: String
+    ): Long
 }
