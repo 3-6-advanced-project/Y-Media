3bbf256ecdc3c31ebcffde8909d71b436756c058
rlaxodud214 <rlaxodud214@naver.com>
2024-05-17 14:54:49 +0900

    feat: API Call 오류 발생 시, Snackbar로 오류 발생 표시
    feat: APi Call Response의 Video 객체 개수에 따라 "검색결과 없음" 또는
    RV가 뜨도록 구현

--- app/src/main/java/com/example/youtubeapi/presentation/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/youtubeapi/presentation/fragment/SearchFragment.kt
@@ -2,6 +2,7 @@ package com.example.youtubeapi.presentation.fragment
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,6 +20,7 @@ import com.example.youtubeapi.presentation.adapter.decoration.ListItemDecoration
 import com.example.youtubeapi.viewmodel.LatestNewsUiState
 import com.example.youtubeapi.viewmodel.MainViewModel
 import com.example.youtubeapi.viewmodel.MainViewModelFactory
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.launch
 
 class SearchFragment : Fragment() {
@@ -30,10 +32,10 @@ class SearchFragment : Fragment() {
         MainViewModelFactory(db.videoDao())
     }
 
-    private val searchListAdapter = SearchListAdapter() { video ->
+    private val searchListAdapter = SearchListAdapter() { videoId ->
         // TODO : 이후 VideoDetailFragment()의 companion object{}에서 parameter를 받도록 수정되면 videoId값 넘겨줘야함
         requireActivity().supportFragmentManager.beginTransaction()
-            // .replace(R.id.fl_item, VideoDetailFragment.newInstance(video.id))
+            // .replace(R.id.fl_item, VideoDetailFragment.newInstance(videoId))
             .replace(R.id.ll_top, VideoDetailFragment())
             .addToBackStack(null)
             .commit()
@@ -87,14 +89,31 @@ class SearchFragment : Fragment() {
             when (uiState) {
                 is LatestNewsUiState.Success -> {
                     val videoStates = uiState.videoStates
+
                     searchListAdapter.submitList(videoStates.toMutableList())
+                    switchVisibility(videoStates.size)
+
                 }
-                is LatestNewsUiState.Error -> initRVItem()
+                is LatestNewsUiState.Error -> showErrorSnackbar(uiState)
             }
         }
     }
 
-    private fun initRVItem() = with(binding) {
+    private fun switchVisibility(size: Int) = with(binding) {
+        if (size > 0) {
+            tvNoData.visibility = View.GONE
+            rvSearch.visibility = View.VISIBLE
+        } else {
+            rvSearch.visibility = View.GONE
+            tvNoData.visibility = View.VISIBLE
+        }
+    }
 
+    private fun showErrorSnackbar(uiState: LatestNewsUiState.Error) = with(binding) {
+        Snackbar.make(requireContext(), binding.root, "Search Error..", Snackbar.LENGTH_SHORT).apply {
+            anchorView = binding.rvSearch
+        }.show()
+
+        Log.e("Search API Error", uiState.exception.message.toString())
     }
 }
