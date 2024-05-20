1f2e4306c74c7a49ad6f627a17e3d1fa439cad47
spartan0226 <161425545+spartan0226@users.noreply.github.com>
2024-05-20 12:21:41 +0900

    MyPage #1 (#16)
    * Create MyVideoAdapter.kt
    
    * Update MyVideoFragment.kt
    
    * Update Constant.kt
    
    * Add files via upload
    
    * Update fragment_my_video.xml
    
    * Add files via upload
    
    * fixed fragment_my_video.xml
    
    * Update MyVideoAdapter.kt

---
+++ app/src/main/java/com/example/youtubeapi/presentation/adapter/MyVideoAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.youtubeapi.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.youtubeapi.data.model.VideoEntity
+import com.example.youtubeapi.databinding.ItemsGridBinding
+
+class MyVideoAdapter(val myVideo: MutableList<VideoEntity>) : RecyclerView.Adapter<MyVideoAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemsGridBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: VideoViewHolder, position: Int) {
+        val dataSet = myVideo[position]
+        holder.thumbnail.load(dataSet.thumbnail.url)
+        holder.duration.text = dataSet.duration
+        holder.title.text = dataSet.title
+        holder.channel.text = dataSet.channelTitle
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getItemCount(): Int {
+        return myVideo.size
+    }
+
+    inner class ViewHolder(private val binding: ItemsGridBinding) : RecyclerView.ViewHolder(binding.root) {
+        val thumbnail = binding.mvRvThumbnail
+        val duration = binding.mvRvDuration
+        val title = binding.mvRvTitle
+        val channel = binding.mvRvChannel
+    }
+}
