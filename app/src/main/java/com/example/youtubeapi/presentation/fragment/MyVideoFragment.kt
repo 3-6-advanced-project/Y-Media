18f9b9729401e6b7750443bad6fa9c57ed18b0a4
treeralph <treeralph@gmail.com>
2024-05-20 14:58:18 +0900

    refactor: MainViewModel input parameter 변경 및 그에 의한 viewModel 선언부 수정

--- app/src/main/java/com/example/youtubeapi/presentation/fragment/MyVideoFragment.kt
+++ app/src/main/java/com/example/youtubeapi/presentation/fragment/MyVideoFragment.kt
@@ -8,19 +8,19 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.youtubeapi.adapter.MyVideoAdapter
-import androidx.lifecycle.lifecycleScope
-import com.example.youtubeapi.R
-import com.example.youtubeapi.bookmarkDummy
-import com.example.youtubeapi.data.model.entity.VideoEntity
+import com.example.youtubeapi.data.local.AppDatabase
 import com.example.youtubeapi.databinding.FragmentMyVideoBinding
 import com.example.youtubeapi.dummyData
 import com.example.youtubeapi.viewmodel.MainViewModel
-import kotlinx.coroutines.launch
+import com.example.youtubeapi.viewmodel.MainViewModelFactory
 
 class MyVideoFragment : Fragment() {
 
     private val binding by lazy { FragmentMyVideoBinding.inflate(layoutInflater) }
-    private val viewModel: MainViewModel by activityViewModels()
+    private val db by lazy { AppDatabase.getInstance(requireContext())!! }
+    private val viewModel: MainViewModel by activityViewModels {
+        MainViewModelFactory(db.videoDao())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
