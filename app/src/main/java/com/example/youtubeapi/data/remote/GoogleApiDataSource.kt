a84cce8d3aafd9169e12a294d09b3ad80af48e57
Sooyoung <agriades23@gmail.com>
2024-05-21 18:56:28 +0900

    feat: 채널 ID 인풋 -> 채널 썸네일 URL, 구독자 수 뱉는 getByChannelId 작성

--- app/src/main/java/com/example/youtubeapi/data/remote/GoogleApiDataSource.kt
+++ app/src/main/java/com/example/youtubeapi/data/remote/GoogleApiDataSource.kt
@@ -73,13 +73,34 @@ interface GoogleApiDataSource {
         @Query("part") part: String = "snippet"
     ): VideoResponse<VideoById>
 
-//    //https://developers.google.com/youtube/v3/docs/channels/list
-//    @GET("youtube/v3/channels")
-//    suspend fun getChannelById(
-//        @Query("key") key: String = BuildConfig.GOOGLE_API_KEY,
-//        @Query("id") id: String, //영상 id인 듯!
-//        @Query("snippet") part: String = "thumbnails", //
-//        @Query("statistics") statistics: String = "subscriberCount",
-//        @Query("maxResults") maxResults: Int
-//    ): VideoResponse<VideoById>
+    //https://developers.google.com/youtube/v3/docs/channels/list
+    /*
+    * 사용할 api: v3/channels. ㅇㅋ
+    * suspend fun getChannelById에서 videoId: String을 받는다.
+    * VideoResponse에 사용할 타입 찾는 중.
+    *
+    * */
+    @GET("youtube/v3/channels")
+    suspend fun getByChannelId(
+        @Query("key") key: String = BuildConfig.GOOGLE_API_KEY,
+        @Query("id") channelId: String, //channel ID
+        @Query("part") part: String = "snippet, statistics"
+    )
 }
+/* [getByChannelId에 대한 주석] - 내가 헷갈려서 기록함
+https://developers.google.com/youtube/v3/docs/channels#properties 에서 각 항목 타입 및 용례를 알아볼 수 있다.
+getByChannelId의 목적은 1. channelId를 input 2. output으로 썸네일 url, 구독자 수 가져오기 뿐이다.
+2-1. 썸네일 가져오기
+* snippet: object
+* snippet.thumbnails: object
+* snippet.thumbnails.default: object
+* ㄴ default, medium, high 중 default(88px * 88px)를 사용하면 된다. 크기가 클 필요가 없기 때문이다.
+* snippet.thumbnails.default.url: string. <
+
+2-2. 구독자 수 가져오기
+* statistics: object
+* statistics.subscriberCount: unsigned long. <
+subscriberCount의 100자리 이상은 반올림되어 있다. 우선은 toString()으로 출력 후,
+다같이 있을 때 "구독자 수" + "000만 명, 0.0천 명" "0.0M Subscribers" 이런 식으로 표현하도록 regex 쓰든지 하자.
+(그런 거 있다는 것만 제목 특수문자 html 처리할 때 보고 어떻게 쓴는지 문법 모름. 배우면 되겠지만... 우선 같이 하기
+*/
