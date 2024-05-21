5131efcf87a94fb4d8ec94d6b3207269fff3b8d0
Sooyoung <agriades23@gmail.com>
2024-05-21 17:25:21 +0900

    refactor: Video.kt 속 channel data class 주석 해제해 Channel로 옮김. 주의: HomeFragment에서도 사용중인 ChannelSnippet에 statistics value 추가함. 아마 nullException 뜰 것이므로 처리해야 함.

--- app/src/main/java/com/example/youtubeapi/data/model/dto/Channel.kt
+++ app/src/main/java/com/example/youtubeapi/data/model/dto/Channel.kt
@@ -13,5 +13,10 @@ data class ChannelSnippet(
     val customUrl: String,
     val publishedAt: String,
     val thumbnails: Thumbnails,
-    val country: String
+    val country: String,
+    val statistics: ChannelStatistics //추가 요소
+) //TODO: HomeFragment에서도 사용중인 ChannelSnippet은 statistics null 처리 필요. DetailFragment에서만 사용 예정
+
+data class ChannelStatistics(
+    val subscriberCount: Long
 )
--- app/src/main/java/com/example/youtubeapi/data/model/dto/Video.kt
+++ app/src/main/java/com/example/youtubeapi/data/model/dto/Video.kt
@@ -24,15 +24,6 @@ data class VideoById(
     val contentDetails: VideoContentDetails, //영상 시간
 )
 
-//data class ChannelByVideoId(
-//    val kind: String,
-//    val etag: String,
-//    val id: String,
-//    val snippet: ChannelSnippet, //에서 channelId, channelTitle, title, description, thumbnails
-//    val contentDetails: VideoContentDetails,
-//)
-
-
 data class Id(
     val kind: String,
     val videoId: String,
@@ -48,13 +39,6 @@ data class VideoSnippet(
     val categoryId: String,
 )
 
-//data class ChannelSnippet(
-//    val title: String,
-//    val description: String,
-//    val customUrl: String,
-//    val thumbnails: Thumbnails, //
-//    val statistics: ChannelStatistics
-//)
 data class VideoContentDetails(
     val duration: String, // PT format: PT23M24S -> 23분 24초
     val dimension: String,
@@ -71,6 +55,24 @@ data class Thumbnail(
     val url: String,
 )
 
+//channel.kt에서도 ChannelSnippet 사용하고 있어 충돌 위험 > channel.kt로 이동
+
+//data class ChannelByVideoId(
+//    val kind: String,
+//    val etag: String,
+//    val id: String,
+//    val snippet: ChannelSnippet,
+//    val contentDetails: VideoContentDetails,
+//)
+//
+//data class ChannelSnippet(
+//    val title: String,
+//    val description: String,
+//    val customUrl: String,
+//    val thumbnails: Thumbnails, //
+//    val statistics: ChannelStatistics
+//)
+//
 //data class ChannelStatistics(
 //    val subscriberCount: Long
 //)
